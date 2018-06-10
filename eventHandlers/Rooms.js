edf520ed0bb8eb06109d45d3010fd7dbba0719b6
jeremy.marchand <jeremy.marchand@batiment.setec.fr>
2018-06-10 19:22:00 +0200

    Add rooms handling (beginning)

--- eventHandlers/Rooms.js
+++ eventHandlers/Rooms.js
@@ -5,24 +5,24 @@ const Rooms = function (io, app, socket) {
 
     this.handler = {
         searchRoom: searchRoom.bind(this),
-        messageRoom: messageRoom.bind(this)
     }
 };
 
 function searchRoom() {
-    console.log('search room')
-    if(true) { //check length room-32 for example
-        this.socket.join('room-' + this.app.counter)
-    }else{
+    var roomName = 'room-' + this.app.counter;
+    var room = this.io.sockets.adapter.rooms[roomName];
+    if(undefined === room ) {       // First co
+        this.socket.join(roomName);
+    }else if(room && room.length < 2){  // Existing room with 1 user
+        this.socket.join(roomName);
+        this.io.sockets.in(roomName).emit('roomFull', roomName);
+    } else{             // None room available
         this.app.counter++;
         this.socket.join('room-'+this.app.counter)
     }
-}
+    this.socket.emit('roomFind', roomName);
+    this.io.sockets.in(roomName).emit('newUserRoom', {});
 
-function messageRoom(room) {
-    const idRoom = room.id;
-    const message = room.message;
-    this.io.sockets.in(idRoom).emit('message', message);
 }
 
 
